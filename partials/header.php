2c53e7493d170818ea0b30770b56527ed0a09390
kholif18 <rahmadnur.kholifatur@gmail.com>
2024-04-14 20:42:32 +0700

    nav menu

--- partials/header.php
+++ partials/header.php
@@ -6,25 +6,25 @@
       </div>
       <nav id="navbar" class="navbar">
         <ul>
-          <li <?php if($page == "Home") echo "class='active'";?>><a class="nav-link scrollto" href="index.php">Home</a></li>
-          <li <?php if($page == "ATK") echo "class='active'";?>><a class="nav-link scrollto" href="atk.php">ATK</a></li>
-          <li <?php if($page == "Banner") echo "class='active'";?>><a class="nav-link scrollto" href="banner.php">Banner</a></li>
-          <li <?php if($page == "Cetak") echo "class='active'";?>><a class="nav-link scrollto" href="cetak.php">Cetak</a></li>
-          <li <?php if($page == "Desain") echo "class='active'";?>><a class="nav-link scrollto" href="desain.php">Desain</a></li>
+          <li><a <?php if($page == "Home") echo "class='active'";?> class="nav-link scrollto" href="index.php">Home</a></li>
+          <li><a <?php if($page == "ATK") echo "class='active'";?> class="nav-link scrollto" href="atk.php">ATK</a></li>
+          <li><a <?php if($page == "Banner") echo "class='active'";?> class="nav-link scrollto" href="banner.php">Banner</a></li>
+          <li><a <?php if($page == "Cetak") echo "class='active'";?> class="nav-link scrollto" href="cetak.php">Cetak</a></li>
+          <li><a <?php if($page == "Desain") echo "class='active'";?> class="nav-link scrollto" href="desain.php">Desain</a></li>
           <li class="dropdown"><a href="#"><span>Form</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
               <li><a href="#">Form Undangan</a></li>
               <li><a href="https://forms.gle/Gvm1t2ZuJx5esX7p7" target="_blank">Form Nama Bayi</a></li>
             </ul>
           </li>
-          <li><a class="nav-link scrollto" href="sablon.php">Sablon</a></li>
-          <li class="dropdown"><a href="#"><span>Undangan</span> <i class="bi bi-chevron-down"></i></a>
+          <li><a <?php if($page == "Sablon") echo "class='active'";?> class="nav-link scrollto" href="sablon.php">Sablon</a></li>
+          <li class="dropdown"><a <?php if($page == "Erba" || $page == "Falah" || $page == "Kabinet" || $page == "Undangan Kita" || $page == "Website") echo "active"; ?> href="#"><span>Undangan</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
-              <li><a href="erba.php">Erba</a></li>
-              <li><a href="falah.php">Falah</a></li>
-              <li><a href="kabinet.php">Kabinet</a></li>
-              <li><a href="undangan-kita.php">Undangan Kita</a></li>
-              <li><a href="website.php">Website</a></li>
+              <li><a <?php if($page == "Erba") echo "class='active'";?> href="erba.php">Erba</a></li>
+              <li><a <?php if($page == "Falah") echo "class='active'";?> href="falah.php">Falah</a></li>
+              <li><a <?php if($page == "Kabinet") echo "class='active'";?> href="kabinet.php">Kabinet</a></li>
+              <li><a <?php if($page == "Undangan Kita") echo "class='active'";?> href="undangan-kita.php">Undangan Kita</a></li>
+              <li><a <?php if($page == "Website") echo "class='active'";?> href="website.php">Website</a></li>
             </ul>
           </li>
           <li><a class="getstarted scrollto" href="https://internet.ravaastudio.my.id" target="_blank">Ravaa.Net</a></li>
